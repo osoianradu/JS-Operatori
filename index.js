ed2d82d2a2fedc3a4d4a886faba354cb612a5a79
osoianradu <122391878+osoianradu@users.noreply.github.com>
2023-01-12 18:17:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+const a1 = 2 + 3* 4 - 5;
+document.writeln("Rezultatul calculului 1 este: ", a1);
+console.log("Rezultatul calculului 1 este: ", a1);
+
+const b2 = 4 * (2 + 6 / 3);
+console.log("Rezultatul calculului 2 este: ", b2);
+document.writeln("Rezultatul calculului 2 este: ", b2);
+const c3 = 2 * 3 ** 2 + 2 ** 5;
+console.log("Rezultatul calculului 3 este: ", c3);
+document.writeln("Rezultatul calculului 3 este: ", c3);
+
+const d4 = (5 * (3 + 2/2)) + 3;
+console.log("Rezultatul calculului 4 este: ", d4);
+document.writeln("Rezultatul calculului 4 este: ", d4);
+
+const e5 = (2 ** 5 - 5 ** 2 + 3 * (5 - 4/2)) / ( 2 ** 5 + 5 ** 2);
+console.log("Rezultatul calculului 5 este: ", e5);
+document.writeln("Rezultatul calculului 5 este: ", e5);
+
+const x = 5;
+const y = 8;
+
+//Ecuatia 1:
+const rezultat1 = 2 * x - 5 * y;
+console.log("Rezultatul ecuatiei este: ", rezultat1);
+document.writeln("Rezultatul ecuatiei este: ", rezultat1);
+//Ecuatia 2:
+const rezultat2 = 3 * x + 9 * y;
+console.log("Rezultatul ecuatiei este: ", rezultat2);
+document.writeln("Rezultatul ecuatiei este: ", rezultat2);
